78c18087641eea6f60ae790e8d59509debd78dc1
Jaundel Simon <jaundelsimon@gmail.com>
2026-06-05 14:05:43 -0400

    Make service worker update live pages immediately

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,18 @@
-const CACHE_NAME = "seven-web-console-v14";
+const CACHE_NAME = "seven-web-console-v15";
 const APP_ASSETS = ["./", "./index.html", "./style.css", "./src/app.js", "./manifest.webmanifest"];
 self.addEventListener("install", (event) => {
   event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_ASSETS)));
+  self.skipWaiting();
 });
 self.addEventListener("activate", (event) => {
-  event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key)))));
+  event.waitUntil(
+    caches.keys()
+      .then((keys) => Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))))
+      .then(() => self.clients.claim())
+  );
 });
 self.addEventListener("fetch", (event) => {
   const requestUrl = new URL(event.request.url);
   if (requestUrl.origin !== self.location.origin) return;
-  event.respondWith(caches.match(event.request).then((cached) => cached || fetch(event.request)));
+  event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
 });
